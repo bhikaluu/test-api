3616c5b5ac0d15d3f80cf4e9497895abbb1bbab7
Aungthu772 <aungthuarun@gmail.com>
2018-02-01 00:14:06 +0630

    'fkdfjkd'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds221148.mlab.com:21148/restdb');
 
 module.exports={
   mongoose
