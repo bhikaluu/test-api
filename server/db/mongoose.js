3e9e032d5a6b9b6b74c073504da1d0815a81c011
Aungthu772 <aungthuarun@gmail.com>
2018-01-31 23:53:35 +0630

    'hellllll'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 const  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const MONGODB_URI="mongodb://<Arun Katwal>:<arunkatwalmLab772772>@ds221148.mlab.com:21148/restdb"
-mongoose.connect(MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports={
   mongoose
